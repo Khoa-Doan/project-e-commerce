d380307c55570cdefe52e0a8e0674e8e40406b25
Khoa Doan <kdoan1991@gmail.com>
2022-05-03 16:47:45 -0700

    Fetch data from commerce.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+import { commerce } from './lib/commerce'
 import { Products, Navbar } from './components';
 
 const App = () => {
-  return (
-    <div>
-        <Navbar />
-        <Products />
-    </div>
+    const [products, setProducts] = useState([]);
+
+    const fetchProducts = async () => {
+        const respone = await commerce.products.list();
+        const { data } = respone;
+
+        setProducts(data);
+    }
+
+    useEffect(() => {
+        fetchProducts();
+    }, [])
+
+    console.log(products);
+
+    return (
+        <div>
+            <Navbar />
+            <Products />
+        </div>
   )
 }
 
