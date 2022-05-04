50d000309b1543eac8659b9d4231ada196f01298
Khoa Doan <kdoan1991@gmail.com>
2022-05-04 00:10:25 -0700

    Add CRUD functionalities for buttons

--- src/App.js
+++ src/App.js
@@ -22,9 +22,27 @@ const App = () => {
     }
 
     const handleAddToCart = async (productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity);
+        const response = await commerce.cart.add(productId, quantity);
         
-        setCart(item);
+        setCart(response.cart);
+    }
+
+    const handleUpdateCartQty = async (productId, quantity) => {
+        const response = await commerce.cart.update(productId, { quantity });
+        
+        setCart(response.cart);
+    }
+
+    const handleRemoveFromCart = async (productId) => {
+        const response = await commerce.cart.remove(productId);
+
+        setCart(response.cart);
+    }
+
+    const handleEmptyCart = async () => {
+        const response = await commerce.cart.empty();
+
+        setCart(response.cart);
     }
 
     useEffect(() => {
@@ -40,7 +58,11 @@ const App = () => {
                 <Navbar totalItems={cart.total_items} />
                 <Routes>
                     <Route exact path='/' element={<Products products={products} onAddToCart={handleAddToCart} />} />
-                    <Route exact path='/cart' element={<Cart cart={cart} />} />
+                    <Route exact path='/cart'
+                        element={<Cart cart={cart}
+                                        handleUpdateCartQty={handleUpdateCartQty}
+                                        handleRemoveFromCart={handleRemoveFromCart}
+                                        handleEmptyCart={handleEmptyCart} />} />
                 </Routes>
             </div>
         </Router>
