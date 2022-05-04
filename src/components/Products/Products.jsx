29441b3b86bd5501c0fa091390d586edf844e024
Khoa Doan <kdoan1991@gmail.com>
2022-05-03 17:03:27 -0700

    Display products (data) fetched from CommerceJS

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,13 +4,7 @@ import { Grid } from '@material-ui/core';
 import Product from "./Product/Product";
 import useStyles from  './styles';
 
-const products = [
-    { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: '' },
-    { id: 2, name: 'Macbook', description: 'Apple macbook.', price: '$10', image: '' },
-    { id: 3, name: 'Window', description: 'Microsoft window.', price: '$15', image: '' },
-]
-
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
 
     return (
